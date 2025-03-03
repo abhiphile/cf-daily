96c567370f34e29396004c0f62a76529219dafcf
Abhishek Kumar <abhishek.nitdelhi@gmail.com>
2025-03-04 02:12:05 +0530

    Wrap up day 2.

---
+++ day3/a.cpp
@@ -0,0 +1,29 @@
+#pragma GCC optimize("Ofast")
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using vi = vector<int>;
+#define fast_io() ios_base::sync_with_stdio(false); cin.tie(nullptr);
+#define all(x) (x).begin(), (x).end()
+#define yes() cout << "YES\n";
+#define no() cout << "NO\n";
+
+void solve() {
+    int n;
+    cin >> n;
+    vi a(n);
+    for (int &x : a) cin >> x;
+
+    sort(all(a));
+
+    for (int x : a) cout << x << " ";
+    cout << '\n';
+}
+
+int main() {
+    fast_io();
+    int t = 1;
+    cin >> t; // Uncomment for multiple test cases
+    while (t--) solve();
+}
